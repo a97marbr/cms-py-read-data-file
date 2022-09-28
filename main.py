6fc0e096586d9ef6cdf37d69dedf1cb5e8a98b4b
Marcus Brohede <a97marbr@users.noreply.github.com>
2022-09-28 13:56:45 +0200

    Read data from .csv file instead of using static arrays

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ import matplotlib.pyplot as plt
 
 
 def example_summary_stats():
-    # list your data directly
-    data1 = [93, 98, 133, 128, 95, 98, 131, 118, 108, 122]
-    data2 = [107, 342, 328, 118, 477, 270, 283, 176, 399, 97]
-    data3 = [167, 199, 261, 135, 113, 246, 159, 225, 157, 229]
-    # Put data into dataframe
-    df = pd.DataFrame()
-    df['Chrome'] = data1
-    df['Explorer'] = data2
-    df['Firefox'] = data3
+    # Read your data from file
+    file = "data.csv"
+    df = pd.read_csv(file, sep=",", header=None, names=['Chrome','Explorer','Firefox'])
 
     print(df.mean())
 
